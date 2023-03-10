5e3482181ba0cdc86ac6fb6db857f1915c7857f5
kyL <t111598038@ntut.org.tw>
2023-03-10 20:03:37 +0800

    feat : 完成環境安裝以及改變Code Style

--- StartScript.bat
+++ StartScript.bat
@@ -1,14 +1,39 @@
 @echo off
 title Simple Neural Network Training Platform
+
+:while
+echo ------------------------------
 echo Choice an option:
 echo 1) Enter Virtual Environment
-echo 2) Environment Setting
+echo 2) Environment Install
 echo 3) Run Flask Server
 echo 4) Exit
+echo ------------------------------
 
-choice /c 1234 /n /m "Input your choice:"
-set choice=%errorlevel%
-if %choice%==1 python -m pipenv shell
-if %choice%==2 echo Environment Setting TBD!
-if %choice%==3 flask run
-if %choice%==4 exit
+set /p choice=Input your choice:
+if %choice%==1 (
+    goto :EnterVirtual
+) else if %choice%==2 (
+    goto :EnvironmentInstall
+) else if %choice%==3 (
+    goto :RunFlask
+) else if %choice%==4 (
+    goto :Finish
+) else (
+    goto :while
+)
+
+:EnterVirtual
+python -m pipenv shell
+goto :while
+
+:EnvironmentInstall
+pipenv install
+goto :while
+
+:RunFlask
+flask Run
+goto :while
+
+:Finish
+echo See you next time!
