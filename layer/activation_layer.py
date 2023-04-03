7e75ea41a5d929052a77dc5eabaa55d53ec80fad
110598015 <t110598015@ntut.org.tw>
2023-04-04 00:13:34 +0800

    feat: Modify Activation_layer class

--- layer/activation_layer.py
+++ layer/activation_layer.py
@@ -1,7 +1,10 @@
 import layer.layer as Layer
 
 class Activation_layer(Layer):
-    def __init__(self, layer_type, type) :
+    def __init__(self, layer_type, layer_dic) :
         super().__init__(layer_type)
-        self.type = type
+        self.type = layer_dic["type"]
+        
+    def get_layer(self)  -> Layer:
+        return layers.Dense(self.type)
         
