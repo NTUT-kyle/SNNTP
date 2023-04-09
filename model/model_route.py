e0561e99d0f6293e13e2aee66251c88b1a6f05f2
kyL <t111598038@ntut.org.tw>
2023-04-09 16:02:14 +0800

    feat : 新增 Model Controller 以 projectName 當作指定哪個 project 底下的 Model

--- model/model_route.py
+++ model/model_route.py
@@ -11,3 +11,23 @@ modelCon = Blueprint("model", __name__)
 @log.log_decorator
 def Index():
     return "You are in model"
+
+@modelCon.route("/<projectName>", methods = ['POST'])
+@log.log_decorator
+def Create_Model(projectName:str):
+    return f'Success POST {projectName} Model'
+
+@modelCon.route("/<projectName>", methods = ['GET'])
+@log.log_decorator
+def Get_Model(projectName:str):
+    return f'Success GET {projectName} Model'
+
+@modelCon.route("/<projectName>", methods = ['PUT'])
+@log.log_decorator
+def Update_Model(projectName:str):
+    return f'Success UPDATE {projectName} Model'
+
+@modelCon.route("/<projectName>", methods = ['DELETE'])
+@log.log_decorator
+def Delete_Model(projectName:str):
+    return f'Success DELETE {projectName} Model'
