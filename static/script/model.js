7e38b1a67c37029dc00e3a9efb4fb6adce43eab7
kyL <t111598038@ntut.org.tw>
2023-04-10 01:11:24 +0800

    feat : Complete first stage for Drag and drop

--- static/script/model.js
+++ static/script/model.js
@@ -1,6 +1,10 @@
 let projectName = $(".MyProject").html();
 
-// Choose data function
+/* 
+
+    Center Board Function
+
+*/
 
 $('#CBFileButton').click(() => {
     $('#CBFile').trigger('click');
@@ -61,7 +65,78 @@ $("#CenterBoardButton").click(() => {
     );
 });
 
+/* 
+
+    Drag and drop 
+
+*/
+let who_are_drag = "";
+$("#Test-1, #Test-2, #Test-3").draggable({
+    start: (event, ui) => {
+        who_are_drag = $(event.target).html();
+    },
+    revert: "invalid",
+    helper: "clone",
+    iframeFix: true,
+    cursor: "move"
+});
+
+let selectItem = null;
+$(".modelshow").droppable({
+    drop: function( event, ui ) {
+        if (who_are_drag == "") return 
+        var targetClass = $(`#${who_are_drag}`).attr('class')
+        var clone = $(ui.helper).clone().addClass(targetClass);
+        clone.click((event) => {
+            if (selectItem != null) {
+                $(selectItem).css('border', 'unset');
+            }
+            selectItem = event.target;
+            $(selectItem).css('border', '1px solid white');
+        });
+        clone.draggable({
+            containment: ".modelshow",
+            scroll: true,
+            cursor: "move",
+            snap: true
+        });
+        $(this).append(clone);
+        who_are_drag = ""
+    }
+});
+
+/* 
+
+    Keydown Event 
+
+*/
+
+$(document).keydown(function(event){
+    if (event.keyCode == 46) { // del
+        if (selectItem != null) {
+            $(selectItem).remove();
+            selectItem = null;
+        }
+    }
+    // console.log("Key: "+event.keyCode);
+});
+
+/* 
+
+    Anywhere Click Event 
+    
+*/
+
 $(document).on("click", function(event) {
+    // Model show item
+    if (selectItem != null) {
+        if (!$(event.target).closest(selectItem).length) {
+            $(selectItem).css('border', 'unset');
+            selectItem = null;
+        }
+    }
+
+    // Center board
     if (!CenterBoard_IsOpen) return;
     let target = '#CenterBoard, #ChooseTrain, #ChooseValidation, #ChooseTest';
     if (!$(event.target).closest(target).length) {
